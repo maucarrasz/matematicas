1e74ef5b7e33354bedd38a34c975d7fe5ba786b3
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-07-01 19:26:13 -0500

    Add input and logic with a coupon platzi with value 5% discount

--- scripts/taller2.js
+++ scripts/taller2.js
@@ -1,17 +1,40 @@
 function calcularPrecioConDescuento(precio, descuento) {
-  return precio - (descuento / 100) * precio;
+  return ((100 - descuento) / 100) * precio;
+}
+function calcularConDescuentoMasCupon(precio, descuento, cupon) {
+  return ((100 - descuento - cupon) / 100) * precio;
 }
 
-function mostrarPrecioConDescuento() {
+function mostrarNuevoPrecio() {
   const inputPrecio = document.getElementById("input-price");
   const inputDescuento = document.getElementById("input-discount");
+  const inputCupon = document.getElementById("input-coupon");
   const textResultado = document.getElementById("answer");
 
   if (inputPrecio.value != 0 && inputDescuento.value != 0) {
-    let resultado = calcularPrecioConDescuento(
-      inputPrecio.value,
-      inputDescuento.value
-    );
+    let resultado = 0;
+    if (inputCupon.value != 0) {
+      // Validar cupon "Platzi"
+      let valorCupon = 0;
+      inputCupon.value = inputCupon.value.toLowerCase();
+      if (inputCupon.value == "platzi") {
+        valorCupon = 5;
+      } else {
+        valorCupon = 0;
+      }
+      console.log(inputCupon.value);
+      console.log(valorCupon);
+      resultado = calcularConDescuentoMasCupon(
+        inputPrecio.value,
+        inputDescuento.value,
+        valorCupon
+      );
+    } else {
+      resultado = calcularPrecioConDescuento(
+        inputPrecio.value,
+        inputDescuento.value
+      );
+    }
     resultado = Math.round((resultado + Number.EPSILON) * 100) / 100;
 
     textResultado.innerHTML = `Resultado final: <span>$${resultado}</span>`;
